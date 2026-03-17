92ffef7d0558ef9087afd28d64bbbeac0d20b32b
kdh0517 <kckk0517@postech.ac.kr>
2026-03-17 23:29:42 +0900

    GitHub Actions 배포 설정 추가

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,16 +19,11 @@ const config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: 'https://kdh0517.github.io',
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'kdh0517',
+  projectName: 'kdh0517.github.io',
 
   onBrokenLinks: 'throw',
 
